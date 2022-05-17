bbdbd03a7cab25c73692f6bd6c783024cb7a0443
Sanjeev <ewb.sanjeev@gmail.com>
2022-05-17 10:56:07 +0530

    Adding calculator and primitve js types

---
+++ tutorial-1/index.js
@@ -0,0 +1,70 @@
+//title variable
+//string rules and methods and properties or funtctions and varibles
+//rule: we can redeclare var but not let and const
+/* var title = 'Hey, It\'s First Tutorial'.length;  //prortery lenght
+var title = 'HEY, IT\'S FIRST TUTORIAL'.toLowerCase();  //method toLowerCase()
+var title = 'hey, it\'s first tutorial'.toUpperCase();   //method toUpperCase()
+var title =' "  Hey , this    is first   tutorial    " '.trim();   //method trim()
+*/
+
+//OPerators
+//Arithmetic
+/* var title = 1+2;
+var title = 1-2;
+var title = 1*2;
+var title = 1/2;
+*/
+
+//Grouping ()
+//var title = (1+2) * 5;
+
+//Concatenation +
+//var title = "hey," + " This is my tutorial";
+
+//functions
+/*
+function gettitle(title)
+{
+     var title;
+     document.write(title);
+}
+*/
+
+//conditions
+/*
+function gettype() {
+    var first = 10;
+    let sec = 20;
+    const third = 10;
+    var four = 10;
+    if (first === sec) {
+        document.write('1');
+    }
+    else if (first === sec) {
+        document.write('2');
+    }
+    else if (four === first) {
+        document.write('4');
+    }
+    else {
+        document.write('3');
+    }
+}
+*/
+
+//Array
+var arr = [
+  {
+      "first" : first,
+      "sec" : sec,
+      "third" : three,
+      "four" : forth,
+      "five" : fivth,
+  }
+];
+
+
+
+
+
+
